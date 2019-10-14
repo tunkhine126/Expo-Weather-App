c7b371c24ad105c73f100cad87313254106fda92
tunkhine126 <tunkhine126@gmail.com>
2019-10-14 16:05:40 -0400

    Setting initial state and loading

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Expo Weather App</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+  
+  render() {
+    const isLoading = this.state
+
+    return (
+      <View style={styles.container}>
+        {isLoading ? (<Text>Fetching Your Weather</Text>) : (
+          <View>
+            <Text>Echo Simple Weather App</Text>
+          </View>  
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
